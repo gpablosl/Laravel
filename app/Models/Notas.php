e96da2ec026ca672a4ccc1b2456afae34e115f07
gpablosl <69810373+gpablosl@users.noreply.github.com>
2022-04-05 09:14:10 -0700

    Update Notas.php

--- app/Models/Notas.php
+++ app/Models/Notas.php
@@ -2,11 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Notas extends Model
 {
-    use HasFactory;
     protected $fillable = ['titulo','contenido'];
 }
